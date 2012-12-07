d6344080447ec330a936c1b76bce5071e098635c
Torben Sko <git@torbensko.com>
2012-12-07 13:55:49 +1100

    Added a simple editor screen. The contents of the CV can now be edited within the browser.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
-$(function() {
+function convert() {
 	var converter = new Showdown.converter();
-	var text = $("body").html();
+	
+	// create an element to place our HTML into
+	var target = $("<div>");
+	$("body").prepend(target);
+
+	var text = $("textarea").val();
 
 	text = text.replace(/\[(\d{4}.*?)\]/g, "<div class=\"year\">$1</div>");
 	text = converter.makeHtml(text);
-	$("body").html(text);
+
+	target.html(text);
+
+	target.append($("<div>", {class: "instructions", text: "Click anywhere to return to the editor. Click print to create a copy of this CV."}))
 
 	// move any images into a div which will be displayed on the first page
 	$("img").parent().addClass("image");
 
 	// split into pages based on horizontal rules
-	$("body").prepend($("<hr>")); // will force a new page
-	$("body").children().each(function() {
+	target.prepend($("<hr>")); // will force a new page
+	target.children().each(function() {
 		var child = $(this);
 
 		if(child.is("hr")) {
 			child.remove();
-			$("body").append(
+			target.append(
 				$("<div>", {class: "page"}).append(
 					$("<div>", {class: "page-content"})
 
@@ -28,8 +36,30 @@ $(function() {
 		}
 	});
 
-	$("body ul").addClass("accolade");
-	var address = $("body ul").first();
+	$("ul", target).addClass("accolade");
+
+	var address = $("ul", target).first();
 	address.addClass("address").removeClass("accolade");
 	$("ul", address).removeClass("accolade");
+
+
+	target.click(function() {
+		target.remove();
+		$("textarea, input").show();
+	});
+}
+
+$(function() {
+
+	var editor = $("<textarea>").val($("body").html());
+	$("body").html("");
+	$("body").append(
+		editor, 
+		$("<input>", {type: "button", value: "Click to preview"}).click(function() {
+			$("textarea, input").hide();
+			convert();
+		})
+	);
+	$("input").click();
+
 });
